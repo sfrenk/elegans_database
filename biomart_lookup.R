aa1d14921fbc9e387dadff148e053ec7fbcb8eef
sfrenk <stephen.frenk@gmail.com>
2016-07-19 16:03:09 -0400

    added feature to allow multiple return arguments

--- biomart_lookup.R
+++ biomart_lookup.R
@@ -40,28 +40,41 @@ in_term <- ifelse(
             opts$query == "transcript", "ensembl_transcript_id", print("ERROR: invalid query value")
             )))
 
-if (opts$result == "coords"){
-    out_term <- c("chromosome_name", "start_position", "end_position", "strand")
-} else {
-    out_term <- ifelse(
-                opts$result == "gene", "ensembl_gene_id", ifelse(
-                opts$result == "wormbase_id", "ensembl_gene_id", ifelse(
-                opts$result == "transcript", "wormbase_gseq", ifelse(
-                opts$result == "type", "gene_biotype", ifelse(
-                opts$result == "description", "description",
-                print("ERROR: invalid result value")
-                )))))
+# The structure below allows for multple "return" results
+out_term <- "ensembl_gene_id"
+
+opts$result <- unlist(strsplit(opts$result, ","))
+
+if ("gene" %in% opts$result){
+    out_term <- c(out_term, "external_gene_id")
 }
+if ("transcript" %in% opts$result){
+    out_term <- c(out_term, "wormbase_gseq")
+}
+if ("type" %in% opts$result){
+    out_term <- c(out_term, "gene_biotype")
+}
+if ("description" %in% opts$result){
+    out_term <- c(out_term, "description")
+}
+if ("coords" %in% opts$result){
+    out_term <- c(out_term, "chromosome_name", "start_position", "end_position", "strand")
+}
+
+print("LOOKING UP:")
+print(out_term)
+print("FROM:")
+print(in_term)
 
 input_data <- read.table(arguments$args, sep = "\t", header = ifelse(opts$header, TRUE, FALSE))
 
 mart <- useMart("parasite_mart", dataset = "wbps_eg_gene", host = "parasite.wormbase.org")
 
-results <- getBM(attributes = c("ensembl_gene_id", out_term), 
+results <- getBM(attributes = c(out_term), 
                                filters = c("species_id_1010", in_term), 
                                values = c("celegans", input_data[opts$c]),
                                mart = mart)
 
-input_data <- cbind(input_data, results)
+#input_data <- cbind(input_data, results)
 
-write.table(input_data, file = opts$output, quote = FALSE, row.names = FALSE, col.names = ifelse(opts$header, TRUE, FALSE))
+write.table(results, file = opts$output, sep = "\t", quote = FALSE, row.names = FALSE)
