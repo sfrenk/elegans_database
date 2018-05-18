f003fbd4fbd3a80a6d37132295a681b8442deece
sfrenk <stephen.frenk@gmail.com>
2018-05-18 12:22:22 -0400

    added briggsae and remani genomes

--- samtools_lookup.py
+++ samtools_lookup.py
@@ -16,25 +16,35 @@ import re
 ###############################################################################
 # VARIABLES
 
-# Fasta file of query genome
-genome_fasta = "/home/sfrenk/Documents/Resources/Seq/WS251/genome/genome.fa"
+# Fasta files for query genomes
+
+# LOCAL
+genome_fasta_files = {"elegans" : "/home/sfrenk/Documents/Resources/Seq/WS251/genome/genome.fa", "briggsae" : "/home/sfrenk/Documents/Resources/Seq/briggsae/wormbase/WS263/genome.fa", "remani" : "/home/sfrenk/Documents/Resources/Seq/remani/genome.fa"}
+
+# CLUSTER
+#genome_fasta_files = {"elegans" : "/nas/longleaf/home/sfrenk/proj/seq/WS251/genome/genome.fa", "briggsae" : "/proj/ahmedlab/steve/seq/briggsae/WS263/caenorhabditis_briggsae.PRJNA10731.WBPS10.genomic.fa", "remani" : "/proj/ahmedlab/steve/seq/briggsae/WS263/caenorhabditis_remanei.PRJNA53967.WBPS10.genomic.fa.gz"}
 
 ###############################################################################
-
-def get_seq(chrom, start, end, name = None, zero = False):
+def get_seq(chrom, start, end, name = None, zero = False, species = "elegans"):
 	
+	# Get coords
 	if zero:
 		# If entry is zero indexed (eg bed file), add 1 to the start coordinate
 		start = int(start) + 1
 
 	coords = str(chrom) + ":" + str(start) + "-" + str(end)
+
+	# Get genome fasta for species
+
+	# Get sequence
+	print("Getting sequence from " + str(species) + " at " + str(chrom) + ":" + str(start) + "-" + str(end) + "\n")
+	genome_fasta = genome_fasta_files[species]
 	seq = subprocess.check_output(["samtools", "faidx", genome_fasta, coords]).decode("utf-8") 
 		
 	if name == None:
 		name = str(re.search(">([^\n]+)\n", seq).group(1))
 
 	# Remove fasta header
-
 	seq = re.sub(">[^\n]*\n", "", seq)
 	seq = re.sub("\n", "", seq).upper()
 
@@ -44,7 +54,7 @@ def get_seq(chrom, start, end, name = None, zero = False):
 	return(seq_record)
 
 
-def iterate_bed(input_file, output_file, zero = True):
+def iterate_bed(input_file, output_file, zero = True, species = "elegans"):
 
 	""" Get sequences for regions in a bed file
 	"""
@@ -68,7 +78,7 @@ def iterate_bed(input_file, output_file, zero = True):
 		except(IndexError):
 			name = None
 
-		output_seq = get_seq(chrom, start, end, name, zero = True)
+		output_seq = get_seq(chrom, start, end, name, zero = True, species = species)
 
 		# Output results in multifasta format
 	
@@ -85,10 +95,11 @@ def main():
 
 	parser.add_argument('file', help = 'file containing regions of interest in bed format')
 	parser.add_argument('-o', '--output', help = 'output file name Default: samtools_results.txt', default = 'samtools_results.txt')
+	parser.add_argument('-s', '--species', help = 'elegans (default), briggsae or remani', default = 'elegans')
 
 	args = parser.parse_args()
 
-	iterate_bed(args.file, args.output)
+	iterate_bed(args.file, args.output, species = args.species)
 
 
 if __name__ == '__main__':
